c22d5776d2803571d4101afbd4d741eff9eb1ebc
Michael Aufreiter <michael@substance.io>
2017-02-08 20:27:31 +0100

    Improved bundling and docs.

--- make.js
+++ make.js
@@ -1,44 +1,42 @@
 var b = require('substance-bundler')
 let path = require('path')
 
+/*
+  Sub-tasks
+*/
 b.task('clean', function() {
   b.rm('./dist')
 })
 
-// copy assets
 b.task('assets', function() {
   b.copy('./node_modules/font-awesome', './dist/font-awesome')
   b.copy('app/index.html', './dist/index.html')
 })
 
-b.task('build-client', ['assets'], function() {
-  b.css('./app/app.css', 'dist/app.css', { variables: true })
-  b.js('app/app.js', {
-    target: {
-      dest: './dist/app.js',
-      format: 'umd',
-      moduleName: 'app'
-    },
-    alias: {
-      'substance': path.join(__dirname, 'node_modules/substance/index.es.js')
-    }
-  })
+/*
+  Core tasks
+*/
+b.task('client', ['assets'], function() {
+  _client(false)
 })
 
-b.task('build-server', function() {
-  b.js('server.js', {
-    external: ['express', 'ws', 'path', 'http'],
-    target: {
-      dest: './server.cjs.js',
-      format: 'cjs',
-      moduleName: 'collab-writer'
-    }
-  })
+b.task('dev:client', ['assets'], function() {
+  _client(true)
 })
 
-b.task('build', ['clean', 'build-client', 'build-server'])
+b.task('server', function() {
+  _server(false)
+})
 
-// build all
+b.task('dev:server', function() {
+  _server(true)
+})
+
+/*
+  Full build tasks
+*/
+b.task('build', ['clean', 'client', 'server'])
+b.task('dev:build', ['clean', 'dev:client', 'dev:server'])
 b.task('default', ['build'])
 
 // starts a server when CLI argument '-s' is set
@@ -46,3 +44,37 @@ b.setServerPort(5555)
 b.serve({
   static: true, route: '/', folder: 'dist'
 })
+
+/*
+  In dev mode no ES5 transforms are made
+*/
+function _client(devMode) {
+  console.info('Building client.... Devmode: ', devMode)
+  console.info('######################################')
+  b.css('./app/app.css', 'dist/app.css', { variables: true })
+  b.js('app/app.js', {
+    target: {
+      dest: './dist/app.js',
+      format: 'umd',
+      moduleName: 'app'
+    },
+    buble: !devMode
+  })
+}
+
+/*
+  In dev mode no ES5 transforms are made
+*/
+function _server(devMode) {
+  console.info('Building server... Devmode: ', devMode)
+  console.info('######################################')
+  b.js('server.js', {
+    external: ['express', 'ws', 'path', 'http'],
+    target: {
+      dest: './server.cjs.js',
+      format: 'cjs',
+      moduleName: 'collab-writer'
+    },
+    buble: !devMode
+  })
+}
